ed2a41b03a9ff250d19d7bd4423eaa7c8f4cebfc
rhw <ruohanw89@gmail.com>
2020-08-08 21:57:59 -0600

    add get id

--- server.js
+++ server.js
@@ -53,6 +53,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -61,10 +66,11 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
   }
-  res.json(results);
 });
